96f741d837593164dfe986c7c14093b775e96c5c
Dev Tavern <admin@devtavern.com>
2018-11-21 00:05:08 +0530

    New method added

--- HelloWorld2/src/Sub2-Same/Sub.java
+++ HelloWorld2/src/Sub2-Same/Sub.java
@@ -2,7 +2,12 @@ public class Sub {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-		String aa = "";
-		
+		String aa = "Ajabu nana";
+		//This is a comment added for testing purpose.
+        helloWorld(aa);
+
+    }
+    public static String helloWorld(String username){
+        retrun username;
     }
 }
