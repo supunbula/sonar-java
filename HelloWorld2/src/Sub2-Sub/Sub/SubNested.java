c87026135d7e7c8ab78c808f21ae687141e910d5
Supun Bulathsinghala <sbulathsinghala@outlook.com>
2018-11-21 00:38:51 +0530

    ERA fixes

--- HelloWorld2/src/Sub2-Sub/Sub/SubNested.java
+++ HelloWorld2/src/Sub2-Sub/Sub/SubNested.java
@@ -2,7 +2,6 @@ public class SubNested {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-		String subnested = "";
-			//This is a comment added for testing purpose.
+		//This is a comment added for testing purpose.
     }
 }
